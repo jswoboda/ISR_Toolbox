c6d2f280ca927442d4d037ba1efb06043868552c
John Swoboda <swoboj@bu.edu>
2013-01-10 18:19:47 -0500

    Added Coordinate transforms for ENU ECEF and WGS.

--- General_Tools/ECI2ECEF.m
+++ General_Tools/ECI2ECEF.m
@@ -24,9 +24,19 @@ function ECEFCOORDS = ECI2ECEF(ECICOORDS,UT)
 % Get the Julian Date
 JD = unix2julian(UT);
 
-T = (JD - 2451545)/36525;
-% Average Greenwich sidreal time in degrees
-GMST = 100.46061837+36000.770053608*T+0.000387933*T.^2-T.^3/38710000;
+% this was taken from the first reference to calculate the GMST.
+JD0 = NaN(size(JD));
+JDmin = floor(JD)-.5;
+JDmax = floor(JD)+.5;
+JD0(JD > JDmin) = JDmin(JD > JDmin);
+JD0(JD > JDmax) = JDmax(JD > JDmax);
+H = (JD-JD0).*24;       %Time in hours past previous midnight
+D = JD - 2451545.0;     %Compute the number of days since J2000
+D0 = JD0 - 2451545.0;   %Compute the number of days since J2000
+T = D./36525;           %Compute the number of centuries since J2000
+%Calculate GMST in hours (0h to 24h) ... then convert to degrees
+GMST = mod(6.697374558 + 0.06570982441908.*D0  + 1.00273790935.*H + ...
+    0.000026.*(T.^2),24).*15;
 
 EPSILONm = 23.439291-0.0130111.*T - 1.64E-07.*(T.^2) + 5.04E-07.*(T.^3);
 
