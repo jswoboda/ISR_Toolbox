681fc29bafa357b32d8985b6b418ebc26939bb2f
Michael <hirsch617@gmail.com>
2015-07-23 01:13:49 -0400

    init

---
+++ Example_Scripts/loadDASC2013Apr14.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from oct2py import Oct2Py
+
+def downloadDASC(dayurl,outdir,timefirstlast):
+    oc = Oct2Py(timeout=300)
+    oc.addpath(oc.genpath('..'))
+    oc.dlFITS(dayurl,outdir,timefirstlast)
+
+
+if __name__ == '__main__':
+    url = 'http://amisr.asf.alaska.edu/PKR/DASC/RAW/2013/20130414/'
+    outdir='c:/data/'
+    timefirstlast=('14-Apr-2013 08:00:00','14-Apr-2013 08:10:00')
+
+    downloadDASC(url,outdir,timefirstlast)
